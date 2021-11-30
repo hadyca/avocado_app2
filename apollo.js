97397b703a04dc1c03f8724bb72f0fd58e0d7753
jhkim5 <jhkim5@ypad.co.kr>
2021-11-30 15:44:48 +0900

    fds

--- apollo.js
+++ apollo.js
@@ -35,7 +35,7 @@ export const logUserOut = async () => {
 // });
 
 const httpLink = createHttpLink({
-  uri: "http://660e-210-108-196-61.ngrok.io/graphql",
+  uri: "http://8e4f-210-108-196-61.ngrok.io/graphql",
 });
 
 const authLink = setContext((_, { headers }) => {
