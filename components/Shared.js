aa12088ba712b16c076daf1d1010b2bf39f1d9b2
jhkim5 <jhkim5@ypad.co.kr>
2022-01-27 16:52:50 +0900

    리팩토링!

--- components/Shared.js
+++
@@ -1,5 +0,0 @@
-import styled from "styled-components/native";
-
-export const FatText = styled.Text`
-  font-weight: bold;
-`;
--- components/auth/Subtitle.js
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components/native";
-import { FatText } from "../Shared";
-
-export const Subtitle = styled(FatText)`
-  font-size: 20px;
-  text-align: center;
-  margin-bottom: 15px;
-`;
