64fa10e9e0866e2445086329355330f30380cdcb
unknown <hadycas@gmail.com>
2022-05-15 18:15:01 +0600

    modal

--- Screens/LoggedIn/Search/CompanyPostAll/CompanyPostAllPresenter.js
+++ Screens/LoggedIn/Search/CompanyPostAll/CompanyPostAllPresenter.js
@@ -1,5 +1,13 @@
-import React from "react";
-import { FlatList, ActivityIndicator } from "react-native";
+import React, { useState } from "react";
+import {
+  FlatList,
+  ActivityIndicator,
+  Modal,
+  Alert,
+  View,
+  TouchableOpacity,
+  Text,
+} from "react-native";
 import styled from "styled-components/native";
 import PostFormButton from "../../../../Components/Post/PostFormButton";
 
@@ -16,8 +24,31 @@ export default function CompanyPostAllPresenter({
   fetchLoading,
   companyOwner,
 }) {
+  const [modalVisible, setModalVisible] = useState(false);
+
   return (
     <>
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          Alert.alert("Modal has been closed.");
+          setModalVisible(!modalVisible);
+        }}
+      >
+        <View>
+          <View>
+            <Text>Hello!</Text>
+            <TouchableOpacity onPress={() => setModalVisible(!modalVisible)}>
+              <Text>Hide Modal</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </Modal>
+      <TouchableOpacity onPress={() => setModalVisible(true)}>
+        <Text>Show Modal</Text>
+      </TouchableOpacity>
       <FlatList
         onEndReachedThreshold={0.05}
         onEndReached={handleFetch}
