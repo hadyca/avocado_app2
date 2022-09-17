7d16f9297ddc94b808c4c7d0db54a986ba66374e
Jae Hyung Kim <SC425@ypsa.com>
2022-09-17 09:55:47 +0600

    fds

--- apollo.js
+++ apollo.js
@@ -44,7 +44,7 @@ export const handleDistrict = async (userId, ...test) => {
 // for simulator
 
 const uploadHttpLink = createUploadLink({
-  uri: "http://9709-182-160-125-147.ngrok.io/graphql",
+  uri: "http://01d2-182-160-125-147.ngrok.io/graphql",
 });
 
 const authLink = setContext((_, { headers }) => {
