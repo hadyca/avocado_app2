f5c0d5e0f60dbdce445ac2be5443607642f00f02
unknown <hadycas@gmail.com>
2022-10-01 18:23:15 +0600

    fds

--- apollo.js
+++ apollo.js
@@ -44,7 +44,7 @@ export const handleDistrict = async (userId, ...list) => {
 // for simulator
 
 const uploadHttpLink = createUploadLink({
-  uri: "https://5601-103-231-176-166.in.ngrok.io/graphql",
+  uri: "https://9065-103-231-176-166.in.ngrok.io/graphql",
 });
 
 const authLink = setContext((_, { headers }) => {
